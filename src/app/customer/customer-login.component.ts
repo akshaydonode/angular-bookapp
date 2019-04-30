07262b1df1920dbed7b65000207fcfad08b6d982
Donode <akshay.donode@capgemini.com>
2019-04-30 10:52:50 +0530

    book app commited firstly

---
+++ src/app/customer/customer-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:"customer-login",
+    templateUrl:"./customer-login.component.html"
+})
+
+export class CustomerLoginComponent{
+
+}
